99a07d1f3ef09832568ae99b496823ed27880467
rosienebc <rosie.baltazar.carvalho@gmail.com>
2019-12-22 14:01:02 -0300

    adicionado api de avisos, feeds e beneficios

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ var knex = require('knex')({
     host: '127.0.0.1',
     user: 'root',
     password: '',
-    database: 'feed'
+    database: 'feeddb'
   }
 });
 
@@ -20,21 +20,214 @@ server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
-server.get('/echo/:name', function(req, res, next) {
-  res.send(req.params);
-  return next();
-});
-
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
 
-//rotas REST
-server.get('/echo/:name', (req, res, next) => {
-  knex('nometabela').then(dados => {
+/* ###############rotas REST FEEDS ############################*/
+server.get('/feeds', (req, res, next) => {
+  knex('feeds').then(dados => {
     res.send(dados);
   }, next);
+});
 
-  res.send(req.params);
-  return next();
+server.post('/feed/create', (req, res, next) => {
+  knex('feeds')
+    .insert(req.body)
+    .then(dados => {
+      res.send(dados);
+    }, next);
+});
+
+server.get('/feed/view/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  knex('feeds')
+    .where('id', id)
+    .first()
+    .then(dados => {
+      if (!dados)
+        return res.send(new erros.BadRequestError('nenhum feed encontrado'));
+      res.send(dados);
+    }, next);
+});
+
+server.put('/feed/update/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  knex('feeds')
+    .where('id', id)
+    .update(req.body)
+    .then(dados => {
+      if (!dados) return res.send(new erros.BadRequestError('erro'));
+      res.send('dados atualizados');
+    }, next);
+});
+
+server.del('/feed/delete/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  knex('feeds')
+    .where('id', id)
+    .delete(req.body)
+    .then(dados => {
+      if (!dados) return res.send(new erros.BadRequestError('erro'));
+      res.send('dados excluidos');
+    }, next);
+});
+
+/* ###############rotas REST AVISOS ############################*/
+server.get('/avisos', (req, res, next) => {
+  knex('avisos').then(dados => {
+    res.send(dados);
+  }, next);
+});
+
+server.post('/avisos/create', (req, res, next) => {
+  knex('avisos')
+    .insert(req.body)
+    .then(dados => {
+      res.send(dados);
+    }, next);
+});
+
+server.get('/avisos/view/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  knex('avisos')
+    .where('id', id)
+    .first()
+    .then(dados => {
+      if (!dados)
+        return res.send(new erros.BadRequestError('nenhum feed encontrado'));
+      res.send(dados);
+    }, next);
+});
+
+server.put('/avisos/update/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  knex('avisos')
+    .where('id', id)
+    .update(req.body)
+    .then(dados => {
+      if (!dados) return res.send(new erros.BadRequestError('erro'));
+      res.send('dados atualizados');
+    }, next);
+});
+
+server.del('/aviso/delete/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  knex('avisos')
+    .where('id', id)
+    .delete(req.body)
+    .then(dados => {
+      if (!dados) return res.send(new erros.BadRequestError('erro'));
+      res.send('dados excluidos');
+    }, next);
+});
+
+/* ###############rotas REST PARCEIROS ############################*/
+server.get('/parceiros', (req, res, next) => {
+  knex('parceiros').then(dados => {
+    res.send(dados);
+  }, next);
+});
+
+server.post('/parceiro/create', (req, res, next) => {
+  knex('parceiros')
+    .insert(req.body)
+    .then(dados => {
+      res.send(dados);
+    }, next);
+});
+
+server.get('/parceiro/view/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  knex('parceiros')
+    .where('id', id)
+    .first()
+    .then(dados => {
+      if (!dados)
+        return res.send(new erros.BadRequestError('nenhum feed encontrado'));
+      res.send(dados);
+    }, next);
+});
+
+server.put('/parceiro/update/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  knex('parceiros')
+    .where('id', id)
+    .update(req.body)
+    .then(dados => {
+      if (!dados) return res.send(new erros.BadRequestError('erro'));
+      res.send('dados atualizados');
+    }, next);
+});
+
+server.del('/parceiro/delete/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  knex('parceiros')
+    .where('id', id)
+    .delete(req.body)
+    .then(dados => {
+      if (!dados) return res.send(new erros.BadRequestError('erro'));
+      res.send('dados excluidos');
+    }, next);
+});
+
+/* ###############rotas REST BENEFICIOS ############################*/
+server.get('/beneficios', (req, res, next) => {
+  knex('beneficios').then(dados => {
+    res.send(dados);
+  }, next);
+});
+
+server.post('/beneficio/create', (req, res, next) => {
+  knex('beneficios')
+    .insert(req.body)
+    .then(dados => {
+      res.send(dados);
+    }, next);
+});
+
+server.get('/beneficio/view/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  knex('beneficios')
+    .where('id', id)
+    .first()
+    .then(dados => {
+      if (!dados)
+        return res.send(new erros.BadRequestError('nenhum feed encontrado'));
+      res.send(dados);
+    }, next);
+});
+
+server.put('/beneficio/update/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  knex('beneficios')
+    .where('id', id)
+    .update(req.body)
+    .then(dados => {
+      if (!dados) return res.send(new erros.BadRequestError('erro'));
+      res.send('dados atualizados');
+    }, next);
+});
+
+server.del('/beneficio/delete/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  knex('beneficios')
+    .where('id', id)
+    .delete(req.body)
+    .then(dados => {
+      if (!dados) return res.send(new erros.BadRequestError('erro'));
+      res.send('dados excluidos');
+    }, next);
 });
